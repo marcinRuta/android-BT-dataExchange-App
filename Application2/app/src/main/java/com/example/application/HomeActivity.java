f08c64e0a6b9cd48cec441e37f6584f3389de12e
pglosek25 <62508503+pglosek25@users.noreply.github.com>
2021-03-20 15:40:09 +0100

    Home z niektórymi funkcjonalnościami

--- Application2/app/src/main/java/com/example/application/HomeActivity.java
+++ Application2/app/src/main/java/com/example/application/HomeActivity.java
@@ -2,13 +2,39 @@ package com.example.application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private int mCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        TextView mTextViewLogout = (TextView) findViewById(R.id.textview_logout);
+        mTextViewLogout.setOnClickListener (view -> {
+            Intent LogoutIntent = new Intent (HomeActivity.this, LoginActivity.class);
+            startActivity(LogoutIntent);
+        });
     }
+
+    @SuppressLint("SetTextI18n")
+    public void SendMessage(View view) {
+        TextView mShowCount = (TextView) findViewById(R.id.msg_count_n);
+        Toast toast = Toast.makeText(this, R.string.sent_message,
+                Toast.LENGTH_SHORT);
+        toast.show();
+        mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+
+    }
+
+
 }
