b72a0665af81e4e224ac2bf279fd636e461835b8
Hubert Maciak <01149553@pw.edu.pl>
2021-03-27 13:27:40 +0100

    Wersja z jedna klasa (dziala)

--- Application2/app/src/main/java/com/example/application/HomeActivity.java
+++ Application2/app/src/main/java/com/example/application/HomeActivity.java
@@ -18,16 +18,39 @@ import android.widget.Toast;
 public class HomeActivity extends AppCompatActivity {
 
     private int mCount = 0;
+    BluetoothAdapter mBluetothAdapter;
     String TAG = "HomeActivity";
-    BluetoothStarter bluetoothStarter = new BluetoothStarter();
 
+    final BroadcastReceiver mBroadcastReceiver1 = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(mBluetothAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, mBluetothAdapter.ERROR);
 
+                switch (state){
+                    case BluetoothAdapter.STATE_OFF:
+                        Log.d(TAG, "onReceive: STATE_OFF");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        Log.d(TAG, "mBroadcastReceiver: STATE_TURNING_OFF");
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+                        Log.d(TAG, "onReceive: STATE_ON");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_ON:
+                        Log.d(TAG, "mBroadcastReceiver: STATE_TURNING_ON");
+                        break;
+                }
+
+            }
+        }
+    };
 
     @Override
     protected void onDestroy(){
         Log.d(TAG, "onDestroy: called.");
         super.onDestroy();
-        unregisterReceiver(bluetoothStarter.mBroadcastReceiver1);
+        unregisterReceiver(mBroadcastReceiver1);
 
     }
 
@@ -35,7 +58,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        bluetoothStarter.setmBluetothAdapter(BluetoothAdapter.getDefaultAdapter());
+        mBluetothAdapter = BluetoothAdapter.getDefaultAdapter();
         TextView mTextViewLogout = (TextView) findViewById(R.id.textview_logout);
         mTextViewLogout.setOnClickListener (view -> {
             Intent LogoutIntent = new Intent (HomeActivity.this, LoginActivity.class);
@@ -49,13 +72,35 @@ public class HomeActivity extends AppCompatActivity {
         btnCollect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent enableBTIntent = new Intent();
-                bluetoothStarter.enableDisableBT(enableBTIntent);
+
+                 enableDisableBT();
             }
         });
     }
 
+    public void enableDisableBT () {
+        if (mBluetothAdapter == null) {
+            Log.d(TAG, "enableDisableBT: Does not have BT capabilities");
+        }
+        if (!mBluetothAdapter.isEnabled()) {
+            Log.d(TAG, "enableDisableBT: enabling Bluetooth");
 
+            Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(enableBTIntent);
+
+            IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(mBroadcastReceiver1, BTIntent);
+        }
+        if (mBluetothAdapter.isEnabled()) {
+            Log.d(TAG, "enableDisableBT: disabling Bluetooth");
+
+            mBluetothAdapter.disable();
+
+            IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(mBroadcastReceiver1, BTIntent);
+        }
+
+    }
 
     @SuppressLint("SetTextI18n")
     public void SendMessage(View view) {
